29e3e1e4b227bc4dcc538a4cec8d09a922f4b788
Javier Parada <javierparada73@outlook.com>
2022-01-02 20:41:59 +0100

    Fix interface.

--- src/contexts/bunker/logs/domain/LogRepository.py
+++ src/contexts/bunker/logs/domain/LogRepository.py
@@ -1,10 +1,10 @@
-from abc import ABC
 from typing import NoReturn
 
 from src.contexts.bunker.logs.domain.entities.Log import Log
+from src.contexts.shared.domain.Interface import Interface
 
 
-class LogRepository(ABC):
+class LogRepository(Interface):
 
     async def create_one(self, log: Log) -> NoReturn:
         raise NotImplementedError()
