157b3069223ccd2d0c27b23bcb7e2db8601492c5
Javier Parada <javierparada73@outlook.com>
2022-02-16 18:55:54 +0100

    Add mongo deps.

--- logbunker/apps/bunker/dependencies/BunkerContainer.py
+++ logbunker/apps/bunker/dependencies/BunkerContainer.py
@@ -10,9 +10,12 @@ from logbunker.contexts.bunker.logs.infrastructure.persistence.elastic.config.El
     ElasticsearchLogConfigFactory
 from logbunker.contexts.bunker.logs.infrastructure.persistence.mongo.PyMongoLogRepository import \
     PyMongoLogRepository
+from logbunker.contexts.bunker.logs.infrastructure.persistence.mongo.config.PyMongoLogConfigFactory import \
+    PyMongoLogConfigFactory
 from logbunker.contexts.shared.Infrastructure.commandbus.InMemoryCommandBus import InMemoryCommandBus
 from logbunker.contexts.shared.Infrastructure.eventbus.InMemoryEventBus import InMemoryEventBus
 from logbunker.contexts.shared.Infrastructure.persistence.elastic.ElasticsearchClientFactory import ElasticsearchClientFactory
+from logbunker.contexts.shared.Infrastructure.persistence.mongo.PyMongoClientFactory import PyMongoClientFactory
 
 
 class BunkerContainer(containers.DeclarativeContainer):
@@ -21,10 +24,10 @@ class BunkerContainer(containers.DeclarativeContainer):
         InMemoryEventBus,
     )
 
-    db_config = providers.Singleton(ElasticsearchLogConfigFactory.create)
-    db_client = providers.Singleton(ElasticsearchClientFactory.create_instance, 'bunker', db_config)
+    db_config = providers.Singleton(PyMongoLogConfigFactory.create)
+    db_client = providers.Singleton(PyMongoClientFactory.create_instance, 'bunker', db_config)
 
-    log_repository = providers.Singleton(ElasticsearchLogRepository, db_client)
+    log_repository = providers.Singleton(PyMongoLogRepository, db_client)
 
     log_creator = providers.Singleton(LogCreator, log_repository, event_bus)
     create_log_command_handler = providers.Singleton(
